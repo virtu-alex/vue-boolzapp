723118b8e82509533f502016d287dede26192d14
virtu-alex <alessio.cordari@gmail.com>
2022-07-14 16:52:32 +0200

    array of objects created

--- js/script.js
+++ js/script.js
@@ -6,7 +6,30 @@ const root = new Vue({
     //prendo l'elemento dal dom(OBBLIGATORIO)
     el: '#root',
     data: {
-       
+        user: {
+            name: 'Alessio',
+            avatar: '_io'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+        ]
+    }, computed: {
+
     },
     methods: {
 
