3db59218c21ec13c5795d61b309d89399549c90e
virtu-alex <alessio.cordari@gmail.com>
2022-07-14 19:34:27 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 console.log('vue ok', Vue)
 
-//RICORDA IL PASCAL CASE
-const root = new Vue({
-    //Posso cambiare il nome all'interno della console (Vue)
-    //prendo l'elemento dal dom(OBBLIGATORIO)
+const root = new Vue({  
     el: '#root',
     data: {
         user: {
