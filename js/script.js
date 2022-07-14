eee49be460f7087d6db8730b61fde527cbd693b9
virtu-alex <alessio.cordari@gmail.com>
2022-07-14 11:57:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('vue ok', Vue)
+
+//RICORDA IL PASCAL CASE
+const root = new Vue({
+    //Posso cambiare il nome all'interno della console (Vue)
+    //prendo l'elemento dal dom(OBBLIGATORIO)
+    el: '#root',
+    data: {
+       
+    },
+    methods: {
+
+    }
+})
